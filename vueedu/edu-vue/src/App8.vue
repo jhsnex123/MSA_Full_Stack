388ce9043f2c71b4a2b29d85f53582d8895c9d6b
hyunsu jang <jhsnex3768@gmail.com>
2024-11-09 16:04:12 +0900

    Vue  이론

---
+++ vueedu/edu-vue/src/App8.vue
@@ -0,0 +1,17 @@
+<script>
+import UserProfile from '@/components/UserProfile8.vue';
+export default {
+  components: {
+    UserProfile,
+  },
+  methods: {
+    printHello(name, age) {
+      alert(`안녕하세요! 저는 ${name}이고, 나이는 ${age}살입니다.`);
+    },
+  },
+};
+</script>
+<template>
+  <!-- 비구조화 할당으로 인자의 값을 받아 처리-->
+  <UserProfile @print-hello="(name, age) => printHello(name, age)" />
+</template>
